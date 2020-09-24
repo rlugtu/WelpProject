7b7657a4e727dcef1a34eebc7b1a1bc5bbb96bfa
Ryan Lugtu <ryanlugtu@Ryans-MacBook-Pro.local>
2020-09-24 16:30:43 -0400

    second push

---
+++ welp/public/precache-manifest.170e2c38f899014c452d2be8dab0baa7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958e44089689616e8be31c9e90e720c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f911f3382177dfdc9c87",
+    "url": "/static/css/2.34c0de18.chunk.css"
+  },
+  {
+    "revision": "402e72ef71fec48a98f4",
+    "url": "/static/css/main.826b4282.chunk.css"
+  },
+  {
+    "revision": "f911f3382177dfdc9c87",
+    "url": "/static/js/2.de01cc2e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.de01cc2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "402e72ef71fec48a98f4",
+    "url": "/static/js/main.c7bc0b7b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
